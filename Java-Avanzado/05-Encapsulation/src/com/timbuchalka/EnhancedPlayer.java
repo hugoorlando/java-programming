6b7f57ac53ad5eabf8cd49884e78bf1cb165f98c
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-30 10:37:46 -0300

    encapsulation

---
+++ Java-Avanzado/05-Encapsulation/src/com/timbuchalka/EnhancedPlayer.java
@@ -0,0 +1,47 @@
+package com.timbuchalka;
+
+public class EnhancedPlayer {
+    private String name;
+    private int health;
+    private String weapon;
+
+    public EnhancedPlayer(String name, int health, String weapon) {
+        this.name = name;
+        if (health >0 && health<=100) {
+            this.health = health;
+        }
+        this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if (this.health <=0) {
+            System.out.println("Player knocked out");
+            // Reduce number of lives remaining for the player
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+}
