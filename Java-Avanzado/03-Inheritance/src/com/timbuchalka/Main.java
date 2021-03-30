d2720ba94d61d0dc7fe879a9701048b2ef234cf4
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-30 10:37:06 -0300

    inheritance

---
+++ Java-Avanzado/03-Inheritance/src/com/timbuchalka/Main.java
@@ -0,0 +1,13 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal animal = new Animal("Animal",1,1,5,5);
+	    Dog dog = new Dog("Pepe", 8, 20,2,4,1,20, "long siky");
+	    dog.eat();
+	    dog.walk();
+	    dog.run();
+	    dog.move(3);
+    }
+}
