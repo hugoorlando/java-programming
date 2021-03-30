81125f76b33933b8e8714799a67c284b8627a9a4
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-30 10:36:24 -0300

    classes

---
+++ Java-Avanzado/01-Classes/src/com/timbuchalka/Main.java
@@ -0,0 +1,16 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porsche = new Car();
+	    Car fiat = new Car();
+	    porsche.setModel("ABC500");
+        System.out.println("Model is " + porsche.getModel());
+
+
+
+
+
+    }
+}
