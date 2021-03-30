bd472a2d695e66adccaad9cef545fe514b71dbb6
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-30 10:38:47 -0300

    references types vs value types

---
+++ Java-Avanzado/08-ReferencesTypesVsValueTypes/src/com/timbuchalka/Main.java
@@ -0,0 +1,30 @@
+package com.timbuchalka;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myIntValue = 10;
+	    int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("anotherIntValue: " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("anotherIntValue: " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+        System.out.println("myIntArray: " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntValue: " + Arrays.toString(anotherArray));
+
+        anotherArray[0] = 1;
+        System.out.println("after change myIntArray: " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherIntValue: " + Arrays.toString(anotherArray));
+
+
+    }
+}
